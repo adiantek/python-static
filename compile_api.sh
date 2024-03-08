bdb15c7cd3d19dabc88edadb48b3123fc75d1e48
Adrian Antkowiak <adiantek@gmail.com>
2024-03-08 23:44:28 +0100

    working

--- compile_api.sh
+++ compile_api.sh
@@ -10,4 +10,4 @@ dir=$1
 gcc -c -o python3api.o python3api.c -I $1/Include/ -I $1/
 cp $1/libpython3.12.a libpython3api.a
 ar rcs libpython3api.a python3api.o
-gcc -o python3api -DPYTHON3API_TEST python3api.c -I $1/Include/ -I $1/ -lm
+gcc -o python3api -DPYTHON3API_TEST python3api.c $1/libpython3.12.a -I $1/Include/ -I $1/ -lm

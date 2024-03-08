b985fe9558106343b1b5a4db5fea18425e6de50a
Adrian Antkowiak <adiantek@gmail.com>
2024-03-08 23:53:58 +0100

    macos 12.0

--- compile_api.sh
+++ compile_api.sh
@@ -6,8 +6,12 @@ if [ -z "$1" ]; then
 fi
 
 dir=$1
+cflags=""
+if [ "$(uname)" == "Darwin" ]; then
+    cflags="-mmacosx-version-min=12.0"
+fi
 
-gcc -c -o python3api.o python3api.c -I $1/Include/ -I $1/
+gcc $cflags -c -o python3api.o python3api.c -I $1/Include/ -I $1/
 cp $1/libpython3.12.a libpython3api.a
 ar rcs libpython3api.a python3api.o
-gcc -o python3api -DPYTHON3API_TEST python3api.c $1/libpython3.12.a -I $1/Include/ -I $1/ -lm
+gcc $cflags -o python3api -DPYTHON3API_TEST python3api.c $1/libpython3.12.a -I $1/Include/ -I $1/ -lm

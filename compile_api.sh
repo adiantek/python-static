226dc2e3fc1dbe24f1e1ced4b72f81044b0b4246
Adrian Antkowiak <adiantek@gmail.com>
2024-03-09 03:07:24 +0100

    working

--- compile_api.sh
+++ compile_api.sh
@@ -17,4 +17,4 @@ fi
 gcc $cflags -c -o python3api.o python3api.c -I $1/Include/ -I $1/
 cp $1/libpython3.12.a libpython3api.a
 ar rcs libpython3api.a python3api.o
-gcc $ldflags $cflags -o python3api -DPYTHON3API_TEST python3api.c $1/libpython3.12.a -I $1/Include/ -I $1/
+gcc $cflags -o python3api -DPYTHON3API_TEST python3api.c $1/libpython3.12.a -I $1/Include/ -I $1/ $ldflags

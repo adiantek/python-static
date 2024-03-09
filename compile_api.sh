7cd060950bc351041831651444c7aeb06a936748
Adrian Antkowiak <adiantek@gmail.com>
2024-03-09 03:02:49 +0100

    working

--- compile_api.sh
+++ compile_api.sh
@@ -9,7 +9,7 @@ fi
 
 dir=$1
 cflags=""
-ldflags="-pthread"
+ldflags="-pthread -lm -ldl"
 if [ "$(uname)" == "Darwin" ]; then
     cflags+=" -mmacosx-version-min=12.0"
 fi
@@ -17,4 +17,4 @@ fi
 gcc $cflags -c -o python3api.o python3api.c -I $1/Include/ -I $1/
 cp $1/libpython3.12.a libpython3api.a
 ar rcs libpython3api.a python3api.o
-gcc $ldflags $cflags -o python3api -DPYTHON3API_TEST python3api.c $1/libpython3.12.a -I $1/Include/ -I $1/ -lm
+gcc $ldflags $cflags -o python3api -DPYTHON3API_TEST python3api.c $1/libpython3.12.a -I $1/Include/ -I $1/
